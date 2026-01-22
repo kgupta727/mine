f2c9c0e9fd66cf79ef925d798860799f3831484b
Kavya Gupta <kgupta1@crimson.ua.edu>
2026-01-22 00:33:43 -0600

    removing json code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,6 @@ import RecommendationCard from '../components/RecommendationCard'
 import RealisticGoalMeter from '../components/RealisticGoalMeter'
 import SmartAlternatives from '../components/SmartAlternatives'
 import { calculateProjection } from '../utils/calculateProjection'
-import { downloadProjectionAsJSON } from '../utils/downloadJson'
 import { formatCurrency, formatWeeksToDuration } from '../utils/formatters'
 
 export default function Home() {
@@ -29,12 +28,6 @@ export default function Home() {
 
   const handleAutoAdjust = () => {
     setAutoAdjustMode(true)
-  }
-
-  const handleExport = () => {
-    if (projection) {
-      downloadProjectionAsJSON(goalName, targetAmount, weeklySavings, projection.dataPoints)
-    }
   }
 
   return (
@@ -134,17 +127,10 @@ export default function Home() {
                 <div className="flex flex-col sm:flex-row gap-3">
                   <button
                     onClick={handleAutoAdjust}
-                    className="flex-1 px-6 py-3 bg-mine-button/20 hover:bg-mine-button/30 text-mine-button font-semibold rounded-2xl
+                    className="w-full px-6 py-3 bg-mine-button/20 hover:bg-mine-button/30 text-mine-button font-semibold rounded-2xl
                                transition-all duration-200 hover:scale-[1.02] border border-mine-button/40 shadow-soft"
                   >
                     Auto-Adjust Budgets
-                  </button>
-                  <button
-                    onClick={handleExport}
-                    className="flex-1 px-6 py-3 bg-gradient-to-r from-mine-primary to-mine-secondary text-white font-semibold rounded-2xl
-                               transition-all duration-200 hover:scale-[1.02] shadow-lg hover:shadow-xl"
-                  >
-                    Export JSON
                   </button>
                 </div>
 
